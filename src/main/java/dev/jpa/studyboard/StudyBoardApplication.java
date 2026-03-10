3fd53f9784a12b1165a145de73b15c8e7a179d4e
seonjiwon <tommys915@gmail.com>
2026-03-10 14:24:58 +0900

    first commit

---
+++ src/main/java/dev/jpa/studyboard/StudyBoardApplication.java
@@ -0,0 +1,13 @@
+package dev.jpa.studyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBoardApplication.class, args);
+	}
+
+}
